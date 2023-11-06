0c8ac7ac5ae09643e92f889a4ffaa07d3df130ff
jose <jose.s.contacto@gmail.com>
2023-11-06 09:31:14 -0300

    Refactor(cli) Create cli class

--- autorg/infrastructure/adapters/cli.py
+++ autorg/infrastructure/adapters/cli.py
@@ -7,39 +7,41 @@ from autorg.application.input import AppInput
 from autorg.infrastructure.adapters.csvrepository import CsvRepository
 
 
-@click.group
-def autorg():
-    pass
+class Cli:
+
+    @click.group
+    def autorg():
+        pass
 
 
-@autorg.group
-def inbox():
-    pass
+    @autorg.group
+    def inbox():
+        pass
 
-@inbox.command(name="add", help="add a input to inbox")
-@click.argument("input_")
-def add_command(input_: str):
-    try:
+    @inbox.command(name="add", help="add a input to inbox")
+    @click.argument("input_")
+    def add_command(input_: str):
+        try:
+            app = AppInput(CsvRepository())
+            app.add_input(input_)
+            click.echo("The input was saved correctly", color=True)
+        except Exception as err:
+            click.echo(click.style(f"Failed adding input, {err}", fg="red"), file=sys.stderr)
+    
+    @inbox.command(name="ls", help="show all inputs in the inbox")
+    @click.option("--date","-d",is_flag=True,default=False,help="show all inputs date creation")
+    def ls_command(date):
         app = AppInput(CsvRepository())
-        app.add_input(input_)
-        click.echo("The input was saved correctly", color=True)
-    except Exception as err:
-        click.echo(click.style(f"Failed adding input, {err}", fg="red"), file=sys.stderr)
-
-@inbox.command(name="ls", help="show all inputs in the inbox")
-@click.option("--date","-d",is_flag=True,default=False,help="show all inputs date creation")
-def ls_command(date):
-    app = AppInput(CsvRepository())
-    inputs: list[str] = [inp for inp in app.list_inputs()]
-
-    if len(inputs) == 0:
-        click.echo("Not inputs found")
-
-    else:
-
-        if not date:
-            for item in inputs:
-                click.echo(item.content)
-        if date:
-            for item in inputs:
-                click.echo(str(item.creation_date)+" "+item.content)
+        inputs: list[str] = [inp for inp in app.list_inputs()]
+    
+        if len(inputs) == 0:
+            click.echo("Not inputs found")
+    
+        else:
+    
+            if not date:
+                for item in inputs:
+                    click.echo(item.content)
+            if date:
+                for item in inputs:
+                    click.echo(str(item.creation_date)+" "+item.content)
--- tests/infrastructure/test_cli.py
+++ tests/infrastructure/test_cli.py
@@ -2,7 +2,8 @@ import os
 import pytest
 import datetime
 from click.testing import CliRunner
-from autorg.infrastructure.adapters.cli import add_command, ls_command
+from autorg.infrastructure.adapters.cli import Cli 
+#add_command, ls_command
 
 # TEST: debo verificar que el input ingresado haya quedado persistido
 # TEST: verificar que los inputs puedan filtrarse por hora/fecha
@@ -13,9 +14,11 @@ from autorg.infrastructure.adapters.cli import add_command, ls_command
 
 class TestCli:
 
+
     def setup_method(self):
         runner = CliRunner()
         self.runner = runner
+        self.cli = Cli()
 
     def teardown_method(self):
         if os.path.exists("data.csv"):
@@ -24,7 +27,7 @@ class TestCli:
 
     @pytest.mark.integration
     def test_input_command_should_persist_data(self):
-        sut = self.runner.invoke(add_command, ["random input"])
+        sut = self.runner.invoke(self.cli.add_command, ["random input"])
         assert sut.output == "The input was saved correctly\n"
 
         # ¿How verify that the input was stored?
@@ -33,8 +36,8 @@ class TestCli:
     @pytest.mark.skip
     #FIX: Deberiamos replantear la forma de la prueba para que sea mas legible
     def test_list_inputs_should_show_a_list_of_all_inboxed_inputs(self):
-        sut = self.runner.invoke(add_command, ["random input"])
-        sut = self.runner.invoke(add_command, ["random input 2"])
+        sut = self.runner.invoke(self.cli.add_command, ["random input"])
+        sut = self.runner.invoke(self.cli.add_command, ["random input 2"])
         sut = self.runner.invoke(ls_command)
         actual =  [+1 for inp in ["random input","random input 2"] if inp in sut.output ]
         assert len(actual) == 2
@@ -42,34 +45,28 @@ class TestCli:
     
     @pytest.mark.integration
     def test_list_inputs_should_show_a_msg_when_there_is_none_inboxed(self):
-        sut = self.runner.invoke(ls_command)
+        sut = self.runner.invoke(self.cli.ls_command)
         assert sut.output == "Not inputs found\n"
         assert sut.exit_code == 0
 
     @pytest.mark.integration
     def test_dont_should_accept_duplicate_inputs(self):
-        self.runner.invoke(add_command, ["input"])
-        self.runner.invoke(add_command, ["input"])
-        sut = self.runner.invoke(ls_command)
+        self.runner.invoke(self.cli.add_command, ["input"])
+        self.runner.invoke(self.cli.add_command, ["input"])
+        sut = self.runner.invoke(self.cli.ls_command)
         assert sut.output == "input\n"
 
     @pytest.mark.integration
     def test_list_inputs_with_d_argument_should_show_each_input_creation_date(self):
-        self.runner.invoke(add_command, ["input"])
-        sut = self.runner.invoke(ls_command,"-d")
+        self.runner.invoke(self.cli.add_command, ["input"])
+        sut = self.runner.invoke(self.cli.ls_command,"-d")
         now = str(datetime.datetime.now())[0:9]
         assert sut.output.startswith(now)
     
     @pytest.mark.integration
     def test_list_inputs_with_fd_option_and_day_should_show_a_filtered_by_day_existing_input_list(self):
-        self.runner.invoke(add_command, ["input"])
-        sut = self.runner.invoke(ls_command,"-d")
-
-
-
-
-
+        self.runner.invoke(self.cli.add_command, ["input"])
+        sut = self.runner.invoke(self.cli.ls_command,"-d")
 
 
     
-
